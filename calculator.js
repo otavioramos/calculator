8a4c2326431645cdc42853a53d285fc4639420be
Otavio Ramos <otavio.omak@gmail.com>
2021-11-01 12:47:43 -0300

    Criando endpoint de post para fazer a soma

--- calculator.js
+++ calculator.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 app.listen(3000, () => {
     console.log("Server is up on port 3000");
+});
+
+app.post("/", (req, res) => {
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const result = num1 + num2;
+    res.send(`The result of calculation is: ${result}`);
 });
 
 app.get("/", (req, res) => {
